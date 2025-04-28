c569dab5fd644f4da9af716fa94e60bd18bc7c99
Shon Juseong <sow7722@gmail.com>
2025-04-29 01:10:57 +0900

    Delete 03_JAVA/src directory

--- 03_JAVA/src/ch06/sec07/exam01/C02Car.java
+++
@@ -1,6 +0,0 @@
-package ch06.sec07.exam01;
-
-public class C02Car {
-    //생성자 선언
-    C02Car(String model, String color, int maxspeed) {}
-}
--- 03_JAVA/src/ch06/sec07/exam01/CarExample.java
+++
@@ -1,8 +0,0 @@
-package ch06.sec07.exam01;
-
-public class CarExample {
-    public static void main(String[] args) {
-        C02Car myCar = new C02Car("그랜저", "검정", 250);
-//        C02Car myCar = new C02Car();  //기본생성자호출못함
-    }
-}
