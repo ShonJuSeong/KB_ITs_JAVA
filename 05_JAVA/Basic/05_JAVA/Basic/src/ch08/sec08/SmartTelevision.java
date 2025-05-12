6cf3368261781f7b39a0f7e9cad5bcb81ae65d16
Shon Juseong <sow7722@gmail.com>
2025-05-12 15:41:55 +0900

    Update SmartTelevision.java

--- 05_JAVA/Basic/05_JAVA/Basic/src/ch08/sec08/SmartTelevision.java
+++ 05_JAVA/Basic/05_JAVA/Basic/src/ch08/sec08/SmartTelevision.java
@@ -1,4 +1,21 @@
 package ch08.sec08;
 
-public class SmartTelevision {
+public class SmartTelevision implements RemoteControl, Searchable{
+        //tunrOn(); 추상 메소드 오버라이딩
+        @Override
+        public void tunrOn(){
+            System.out.println("TV를 켭니다.");
+        }
+
+        //turnOff(); 추상메소드 오버라이딩
+        @Override
+        public void tunrOff(){
+            System.out.println("TV를 끕니다.");
+        }
+
+        //search() 추상메소드 오버라이딩
+        @Override
+        public void search(String url) {
+            System.out.println(url + "을 검색합니다.");
+        }
 }
