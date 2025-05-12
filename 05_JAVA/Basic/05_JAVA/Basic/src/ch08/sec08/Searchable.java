2f463a2359d0cac1adc889e473e4a1c2926e1b17
Shon Juseong <sow7722@gmail.com>
2025-05-12 15:42:53 +0900

    Update Searchable.java

--- 05_JAVA/Basic/05_JAVA/Basic/src/ch08/sec08/Searchable.java
+++ 05_JAVA/Basic/05_JAVA/Basic/src/ch08/sec08/Searchable.java
@@ -1,23 +1,6 @@
 package ch08.sec08;
 
-import org.w3c.dom.ls.LSOutput;
-
-public interface Searchable implements RemoteControl, Searchable{
-    //tunrOn(); 추상 메소드 오버라이딩
-    @Override
-    public void tunrOn(){
-        System.out.println("TV를 켭니다.");
-    }
-
-    //turnOff(); 추상메소드 오버라이딩
-    @Override
-    public void tunrOff(){
-        System.out.println("TV를 끕니다.");
-    }
-
-    //search() 추상메소드 오버라이딩
-    @Override
-    public void search(String url) {
-        System.out.println(url + "을 검색합니다.");
-    }
+public interface Searchable{
+    //추상 메소드
+    void search(String url);
 }
