30fd2bf3d7bf97dd94e6ebf6b74a8c6b19537c30
Shon Juseong <sow7722@gmail.com>
2025-05-14 18:23:38 +0900

    Create E02_Exam03.java

---
+++ 02_JAVA/Deep/E02_Exam03.java
@@ -0,0 +1,17 @@
+package Deep;
+/*
+ *****
+ ****
+ ***
+ **
+ *
+ */
+public class E02_Exam03 {
+    public static void main(String[] args) {
+        for(int i = 5; i >= 1; i--){
+            for(int k = 1; k <= i; k++)
+            {System.out.print("*");}
+            System.out.println();
+        }
+    }
+}
