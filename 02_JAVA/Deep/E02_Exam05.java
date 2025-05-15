b392eb74a610b8cac1949296775c54de9e5ae358
Shon Juseong <sow7722@gmail.com>
2025-05-15 12:49:09 +0900

    Create E02_Exam05.java

---
+++ 02_JAVA/Deep/E02_Exam05.java
@@ -0,0 +1,13 @@
+package Deep;
+
+public class E02_Exam05 {
+    public static void main(String[] args) {
+        int[][] array = {
+                {95, 86}, {83, 92, 96}, {78, 83, 93, 87, 88}
+        };
+        System.out.println(array.length);
+        System.out.println(array[2].length);
+    }
+}
+// 3 총 배열의 길이 -> 3개니깐 3 나오는게 맞음
+// 5 -> 0, 1, 2번째 배열의 길이니깐, 5 나오는게 맞음
